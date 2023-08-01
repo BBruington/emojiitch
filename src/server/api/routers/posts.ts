b28af54f8e47867f0b7dc597ac1fb133f21afb24
BIBruington <psychological_chemist@hotmail.com>
2023-08-01 17:53:20 -0500

    home page / get posts api

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,14 +1,39 @@
 import { z } from "zod";
+import type {User} from "@clerk/nextjs/api"
 import {
   createTRPCRouter,
   publicProcedure,
   protectedProcedure,
 } from "npm/server/api/trpc";
+import { clerkClient } from "@clerk/nextjs";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id, 
+    username: user.username, 
+    profileImageUrl: user.profileImageUrl, 
+  }
+ }
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit:100,
+    })).map(filterUserForClient)
+
+    console.log(users)
+
+    return posts.map(post => ({
+      post,
+      author: users.find((user) => user.id === post.authorId),
+    }))
+
   }),
 
   getSecretMessage: protectedProcedure.query(() => {
