0c0766eeafa8a4d0bb65d17ad9b8bb90ab81ca0c
BIBruington <psychological_chemist@hotmail.com>
2023-08-05 21:21:44 -0400

    loading spinner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { signIn, signOut, useSession } from "next-auth/react";
+import { LoadingSpinner } from "npm/components/loading";
 import Head from "next/head";
 import Link from "next/link";
 import type { RouterOutputs } from "npm/utils/api";
@@ -69,7 +69,7 @@ export default function Home() {
 
   const {data, isLoading} = api.posts.getAll.useQuery()
 
-  if(!data || isLoading) return <div>Loading...</div>
+  if(!data || isLoading) return <LoadingSpinner />
 
   if(!data) return <div>Something went wrong</div>
 
@@ -101,26 +101,3 @@ export default function Home() {
   );
 }
 
-// function AuthShowcase() {
-//   const { data: sessionData } = useSession();
-
-//   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-//     undefined, // no input
-//     { enabled: sessionData?.user !== undefined }
-//   );
-
-//   return (
-//     <div className="flex flex-col items-center justify-center gap-4">
-//       <p className="text-center text-2xl text-white">
-//         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-//         {secretMessage && <span> - {secretMessage}</span>}
-//       </p>
-//       <button
-//         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-//         onClick={sessionData ? () => void signOut() : () => void signIn()}
-//       >
-//         {sessionData ? "Sign out" : "Sign in"}
-//       </button>
-//     </div>
-//   );
-// }
